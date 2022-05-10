c0d831f8657e07d25ef81b9eda38662ad1e3d7fb
dasta <arzymatodastan@gmail.com>
2022-05-10 11:57:18 +0600

    homework 11 main and triangle

--- src/Mashina.java
+++ src/Mashina.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Mashina{
     String name;
+    String model;
+    String colour;
     int jyly;
     int kolomu;
     String kuzov;
+
+
+
+
+
+
+void chek(int a){
+
+if ( a<=60){
+    System.out.println("normal");
+}else if (a>=120){
+    System.out.println("maxx");
+}else {
+    System.out.println("no");
 }
+}
+}
+
